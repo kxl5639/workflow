e4234a5b585d6c84009dfc2124f501a539c470f9
kxl5639 <74498576+kxl5639@users.noreply.github.com>
2024-06-30 09:34:22 -0400

    Title Manager
    Able to track recently active entry widget to get data to move up or down

--- title_controller.py
+++ title_controller.py
@@ -1,9 +1,15 @@
 from title_view import TitleView
 
 class TitleController:
-    def __init__(self, parent) -> None:
+    def __init__(self, parent=None) -> None:
         self.parent = parent
         self.view = TitleView('Title Manager', self.parent, self)
 
     def add_entry(self, parent):
         self.view.create_entry_widget(parent)
+
+    def moveup_entry(self):
+        self.view.moveup_entry()
+
+    def movedown_entry(self):
+        self.view.movedown_entry()
--- title_view.py
+++ title_view.py
@@ -10,6 +10,8 @@ class TitleView:
         self.title = title
         self.parent = parent
         self.controller = controller
+        self.title_column_break = 10
+        self.active_entry_widget = None
 
         if __name__ == '__main__':
             self.root = BaseWindow(self.title, self.parent, is_root=isroot).root
@@ -18,7 +20,7 @@ class TitleView:
 
         self.root.resizable(width=True, height=True)
 
-        # Create frame where title entries and menu buttons will live
+        # Create base frame where title entries and menu buttons will live
         self.base_frame = ttk.Frame(self.root)
         self.base_frame.grid(row=0, column=0, sticky='nsew')
         self.base_frame.grid_columnconfigure(0, weight = 1)
@@ -30,7 +32,8 @@ class TitleView:
         self.entries_frame.grid_columnconfigure(0, weight = 1)
 
         # Create initial title entry object
-        self.create_entry_widget(self.entries_frame)        
+        for i in range(1,self.title_column_break+1):
+            self.create_entry_widget(self.entries_frame)        
 
         # Create frame for menu buttons
         self.menu_frame = ttk.LabelFrame(self.base_frame, text='Menu')
@@ -39,9 +42,10 @@ class TitleView:
         # Create button in menu frame
         self.add_button = create_button_frame(self.menu_frame,[('Add', lambda:self.controller.add_entry(self.entries_frame))])
         self.add_button.grid(row=0, column=0, padx=(10), pady = 10)
-
-        self.moveup_button = create_button_frame(self.menu_frame,[('Move Up', None)])
+        self.moveup_button = create_button_frame(self.menu_frame,[('Move Up', lambda:self.controller.moveup_entry())])
         self.moveup_button.grid(row=1, column=0, padx=(10), pady = (0,10))
+        self.movedown_button = create_button_frame(self.menu_frame,[('Move Down', lambda:self.controller.movedown_entry)])
+        self.movedown_button.grid(row=2, column=0, padx=(10), pady = (0,10))
 
         center_window(self.root)
         self.root.focus_force()
@@ -57,27 +61,41 @@ class TitleView:
             return count
         
         count = count_entries_recursive(self.root)
-        print(f"Number of entry widgets: {count}")
         return count
 
     def create_entry_widget(self, parent):
         '''Creates entry widgets'''
         # Check how many existing entry widgets there are
         entry_count = self._count_entry_widgets()
-        column_break = 10
-        in_column = entry_count // column_break
-        in_row = entry_count - (in_column*column_break)
+        in_column = entry_count // self.title_column_break
+        in_row = entry_count - (in_column*self.title_column_break)
         entry_frame = ttk.Frame(parent)
-        ypad = 10 if entry_count % column_break == 0 else (0,10) 
+        ypad = 10 if entry_count % self.title_column_break == 0 else (0,10) 
         entry_frame.grid(row=in_row, column=in_column, padx=10, pady=ypad, sticky='nsew')
         entry_frame.grid_columnconfigure(1, weight = 1)
         entry_label = ttk.Label(entry_frame, text=entry_count+1)
         entry_label.grid(row=0, column=0, padx=(0,5))
         entry = ttk.Entry(entry_frame)
         entry.grid(row=0, column=1, sticky='nsew')
-        if entry_count % column_break == 0 or entry_count == column_break-1:
-            print('We center')
+        entry.bind("<FocusIn>", lambda event: self._get_active_entry_widget(entry))
+        if entry_count % self.title_column_break == 0 or entry_count == self.title_column_break-1:
             center_window(self.root)
+
+    def _get_active_entry_widget(self, entry):
+        self.active_entry_widget = entry
+
+    def moveup_entry(self):
+        # Get data from active entry widget
+        try:
+            curr_data = self.active_entry_widget.get()
+        except AttributeError: print('Nothing Selected')
+        else:
+            print(curr_data)
+        # Get data from entry widget above
+        
+
+    def movedown_entry(self):
+        pass
 
 
 if __name__ == '__main__':
@@ -85,5 +103,7 @@ if __name__ == '__main__':
         isroot = True
     else:
         isroot = False
-    root = TitleView('Title Manager', None).root
+    from title_controller import TitleController
+    controller = TitleController()
+    root = TitleView('Title Manager', None, controller).root
     root.mainloop()
