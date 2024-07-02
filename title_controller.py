0ab80d96576a8199714e08ba2d39f4d68f0f084e
kxl5639 <74498576+kxl5639@users.noreply.github.com>
2024-07-02 11:53:28 -0400

    Adding docstrings

--- title_controller.py
+++ title_controller.py
@@ -10,26 +10,33 @@ class TitleController:
 
 #region View
     def add_entry(self, parent):
+        """Add a new entry widget to the view."""
         self.view.create_entry_widget(parent)
 
     def moveup_entry(self):
+        """Move the selected entry widget up."""
         self.view.move_entry('up')
 
     def movedown_entry(self):
+        """Move the selected entry widget down."""
         self.view.move_entry('down')
     
     def on_project_combobox_selected(self):
+        """Handle project combobox selection."""
         self.view.on_project_selected()
 #endregion
 
 #region Model
     def get_project_object(self, project_number):
+        """Get project object from the model."""
         return self.model.get_project_object(project_number)
     
     def get_title_object(self, project_object):
+        """Get title object list from the model."""
         return self.model.get_title_object(project_object)
     
     def update_new_page_title_dict(self):
+        """Update the new page title dictionary and return it along with pages to be deleted."""
         entry_widget_list = self.view.get_all_entry_widgets(self.view.root)
         new_titles, removed_indices = self.model._remove_end_blanks([title.get() for title in entry_widget_list])    
         pages_to_be_deleted_from_screen = [item+1 for item in removed_indices]
@@ -37,19 +44,18 @@ class TitleController:
         return new_page_title_dict, pages_to_be_deleted_from_screen
     
     def commit_titles(self, project_number): 
-
+        """Commit titles to the model."""
         project_obj = self.get_project_object(project_number)
         existing_title_record_objs = self.get_title_object(project_obj)        
-        new_page_title_dict, pages_to_be_deleted_from_screen = self.update_new_page_title_dict()
-        existing_page_titleobj_dict = {existing_title_record_objs.index(title_obj)+1 : title_obj for title_obj in existing_title_record_objs}
+        new_page_title_dict, pages_to_be_deleted_from_screen = self.update_new_page_title_dict()        
+        existing_page_titleobj_dict = {idx + 1: title_obj for idx, title_obj in enumerate(existing_title_record_objs)}
         
         # Destroy the end title entry widgets that are blank (view)
         if pages_to_be_deleted_from_screen != []:
             self.view.destroy_frames_if_labels_match(pages_to_be_deleted_from_screen)
-            new_page_title_dict, pages_to_be_deleted_from_screen = self.update_new_page_title_dict()            
-            print(f'\nNew page title dict: {new_page_title_dict.items()}')
+            new_page_title_dict, pages_to_be_deleted_from_screen = self.update_new_page_title_dict()                        
 
-        # Get list of title objects to be deleted as well as entry widgets that need to be popped
+        # Get list of title record objects to be deleted as well as entry widgets that need to be popped
         title_record_obj_to_delete = []
         for existing_page_number in list(existing_page_titleobj_dict.keys()):
             if existing_page_number not in new_page_title_dict:
@@ -63,12 +69,12 @@ class TitleController:
         for new_page_number, new_title_name in new_page_title_dict.items():
             if new_page_number in existing_page_titleobj_dict:
                 # This page already exist. We are just updating the existing title name
-                existing_page_titleobj_dict[new_page_number].title = new_title_name
-                # print(f'DWG-{new_page_number}: {existing_page_titleobj_dict[new_page_number].title} --> {new_title_name}')
+                existing_page_titleobj_dict[new_page_number].title = new_title_name                
             else:
-                # This is a new page that we are adding
-                # print(f'DWG-{new_page_number} title to be added: {new_title_name}')
-                new_title_record = DwgTitle(dwgno=new_page_number, title = new_title_name, project_id=project_obj.id)
+                # This is a new page that we are adding                
+                new_title_record = DwgTitle(dwgno=new_page_number,
+                                            title = new_title_name,
+                                            project_id = project_obj.id)
                 self.model.add_record(new_title_record)
 
         # Commit changes
--- title_model.py
+++ title_model.py
@@ -3,16 +3,17 @@ from model import session, Project, DwgTitle
 class TitleModel:
 
     def get_project_object(self, project_number):
+        """Retrieve a project object based on the project number."""
         return session.query(Project).filter_by(project_number=project_number).first()
         
     def get_title_object(self, project_object):
-        '''RETURNED TITLE OBJECT LIST IS SORTED BY DRAWING NUMBER'''
+        """Return a list of title objects sorted by drawing number."""
         return session.query(DwgTitle).filter_by(project_id=project_object.id).order_by(DwgTitle.dwgno).all()
 
     def delete_title_record(self, existing_page_titleobj_dict, new_page_title_dict):
+        """Delete title records that are not in the new page title dictionary."""
         for existing_page_number, existing_title_obj in existing_page_titleobj_dict.items():
-            if existing_page_number not in new_page_title_dict:
-                print(f'{existing_title_obj.title} to be deleted')
+            if existing_page_number not in new_page_title_dict:                
                 session.delete(existing_title_obj)
 
     def delete_record(self, object_list):
@@ -30,19 +31,14 @@ class TitleModel:
         # Check if the last item is a blank
         if list_obj and list_obj[-1] != '':
             print(f'\nLast object not blank: {list_obj}')
-            return list_obj, []  # If the last item is not a blank, return the original list and an empty list of removed indices
-        
-        # Find the first non-blank item from the end of the list
-        last_non_blank_index = len(list_obj)
-        
+            return list_obj, []  # If the last item is not a blank, return the original list and an empty list of removed indices                
+        last_non_blank_index = len(list_obj) # Find the first non-blank item from the end of the list
         for index in range(len(list_obj) - 1, -1, -1):
             if list_obj[index] != '':
                 last_non_blank_index = index + 1
-                break
-        
+                break        
         # Calculate the indices of the removed blank items
-        removed_indices = list(range(last_non_blank_index, len(list_obj)))
-        
+        removed_indices = list(range(last_non_blank_index, len(list_obj)))        
         # Slicing to remove trailing blanks
         modified_list = list_obj[:last_non_blank_index]
         print(f'\nModified list: {modified_list}')
