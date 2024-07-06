f5bb421033b85c85cdd8bbaaf368d993513e8904
kxl5639 <74498576+kxl5639@users.noreply.github.com>
2024-07-05 22:58:09 -0400

    ProjectDetail
    - Accounted for projects that don't yet have systems or devices

--- project_detail/projectdetail_view.py
+++ project_detail/projectdetail_view.py
@@ -25,17 +25,27 @@ class ProjectDetailWindow(BaseWindow):
         self.system_base_frame.grid(row=1, column=0,padx=10,pady=(0,10))
         return self.system_base_frame
 
-    def create_system_frame(self, row_idx, system_name, system_id):        
+    def create_system_frame(self, row_idx, system_name, system_id=None):        
         system_frame = ttk.LabelFrame(self.system_base_frame, text=system_name)
         ypad = 10 if row_idx == 0 else (0,10)
-        system_frame.grid(row=row_idx, column=0, padx=10, pady=ypad)
+        system_frame.grid(row=row_idx, column=0, padx=10, pady=ypad)        
         device_base_frame = self.create_device_base_frame(system_frame)
         self.iter_generate_device_frame(device_base_frame, system_id)
 
+    def create_add_system_frame(self, row_idx):
+        system_frame = ttk.LabelFrame(self.system_base_frame, text='Add System')
+        ypad = 10 if row_idx == 0 else (0,10)
+        system_frame.grid(row=row_idx, column=0, padx=10, pady=ypad)
+        system_add_label = ttk.Label(system_frame, text='Click here to add a new system.')      
+        system_add_label.grid(row=0,column=0,padx=10,pady=10)
+
     def iter_generate_system_frame(self):        
         system_names, system_ids = self.controller.get_systems_names_ids_list()
-        for idx, (system_name, system_id) in enumerate(zip(system_names, system_ids)):
-            self.create_system_frame(idx,system_name, system_id)
+        if system_names:
+            for idx, (system_name, system_id) in enumerate(zip(system_names, system_ids)):
+                self.create_system_frame(idx,system_name, system_id)
+        else:
+            self.create_add_system_frame(0)
 
     def create_device_base_frame(self, parent):
         device_base_frame = ttk.LabelFrame(parent, text='Devices')
@@ -49,8 +59,23 @@ class ProjectDetailWindow(BaseWindow):
         device_label = ttk.Label(device_frame, text=device_name)
         device_label.grid(row=0,column=0)
 
+    # def create_add_device_frame(self, parent, row_idx):
+    #     device_frame = ttk.LabelFrame(parent, text='Add Device')
+    #     ypad = 10 if row_idx == 0 else (0,10)
+    #     device_frame.grid(row=row_idx, column=0, padx=10, pady=ypad)
+    #     device_add_label = ttk.Label(device_frame, text='Click here to add a new device.')      
+    #     device_add_label.grid(row=0,column=0,padx=10,pady=10)
+
+    def create_add_device_frame(self, parent, row_idx):        
+        ypad = 10 if row_idx == 0 else (0,10)
+        device_add_label = ttk.Label(parent, text='Click here to add a new device.')      
+        device_add_label.grid(row=0,column=0,padx=10,pady=ypad)
+
     def iter_generate_device_frame(self, parent, system_id):
         devices_names, devices_ids = self.controller.get_devices_ids_list(system_id)
-        for idx, (device_name, device_id) in enumerate(zip(devices_names, devices_ids)):
-            self.create_device_frame(parent, idx, device_name)
+        if devices_names:
+            for idx, (device_name, device_id) in enumerate(zip(devices_names, devices_ids)):
+                self.create_device_frame(parent, idx, device_name)
+        else:
+            self.create_add_device_frame(parent, 0)
 
