1ec58dab3306d185501db8d4209d864fd0422285
kxl5639 <74498576+kxl5639@users.noreply.github.com>
2024-07-06 15:46:43 -0400

    ProjectDetail
    - Window loads with all necessary info.
    - Need to fix when there are no devices. Right now the condition is always true and it always thinks there are devices.

--- project_detail/projectdetail_controller.py
+++ project_detail/projectdetail_controller.py
@@ -12,50 +12,40 @@ class ProjectDetailController:
         self.view = ProjectDetailWindow(f'{project_number} Project Detail',
                                         parent, self, project_number)
     
-    def get_systems_names_ids_list(self):
-        proj_id = self.model.get_id_from_model_column_data(Project, 'project_number', self.project_number)
-        systems_names = self.get_child_names_list(System, proj_id, 'project_id')        
-        systems_ids = self.get_child_ids_list(System, proj_id, 'project_id')
-        return systems_names, systems_ids
-
-    def get_max_devices_data_char(self, systems_devices_data_dict, system_key):
-        system_id = system_key[0]
-        for cat in systems_devices_data_dict[system_id].keys():
-            if cat not in self.max_device_data_char_dict:
-                self.max_device_data_char_dict[cat] = 0
-            for next in systems_devices_data_dict[system_id][cat]['data']:
-                if cat == 'devices_qtys':
-                    self.max_device_data_char_dict[cat] = 4
-                else:
-                    if len(next) > self.max_device_data_char_dict[cat]:
-                        self.max_device_data_char_dict[cat] = len(next)
-    
     def get_systems_devices_data(self):
         # Gets all systems keys of the project number
         systems_keys = self.get_systems_keys()
         # Gets all the device data (tag, desc, manf, etc...) for each system_key
             # Also gets the max character length for each device data
         for system_key in systems_keys:
-            self.get_devices_data(system_key[0])
+            self.get_devices_data(system_key)
             self.get_max_devices_data_char(self.systems_devices_data_dict, system_key)
         # Update max char for each systems_devices_data_dict
         for system_key in systems_keys:
-            system_id = system_key[0]
-            for device_prop_key in self.systems_devices_data_dict[system_id]:
-                self.systems_devices_data_dict[system_id][device_prop_key]['max_char']=self.max_device_data_char_dict[device_prop_key]
-        print(f'\n{self.systems_devices_data_dict[1]}')
-        print(f'\n{self.systems_devices_data_dict[2]}')
+            # system_id = system_key[0]
+            for device_prop_key in self.systems_devices_data_dict[system_key]:
+                self.systems_devices_data_dict[system_key][device_prop_key]['max_char']=self.max_device_data_char_dict[device_prop_key]
+        return self.systems_devices_data_dict
+    
+    def get_max_devices_data_char(self, systems_devices_data_dict, system_key):        
+        for cat in systems_devices_data_dict[system_key].keys():
+            if cat not in self.max_device_data_char_dict:
+                self.max_device_data_char_dict[cat] = 0
+            for next in systems_devices_data_dict[system_key][cat]['data']:
+                if cat == 'devices_qtys':
+                    self.max_device_data_char_dict[cat] = 5
+                else:
+                    if len(next) > self.max_device_data_char_dict[cat]:
+                        self.max_device_data_char_dict[cat] = len(next)
 
     def get_systems_keys(self):
         '''Generates the key as a tuple [ex: (1, 'AHU')] for self.systems_devices_data_dict.'''
         systems_ids = self.get_systems_ids_from_proj_num()
-        systems_names = []
         systems_keys = []
         for idx, system_id in enumerate(systems_ids):
             system_obj = self.model.get_objs_from_column_data(System,'id',system_id)
             system_name = system_obj[0].name
             systems_keys.append((system_id, system_name))
-        print(f'\nsystem keys: {systems_keys}')
         return systems_keys
 
     def get_systems_ids_from_proj_num(self):
@@ -66,7 +56,8 @@ class ProjectDetailController:
             systems_ids.append(system_obj.id)
         return systems_ids
 
-    def get_devices_data(self, system_id):
+    def get_devices_data(self, system_key):
+            system_id = system_key[0]
             systemdevices_ids = self.get_child_ids_list(SystemDevice, system_id, 'system_id')
             devices_ids = self.get_target_col_vals_list_by_known_col_val(SystemDevice,'id', systemdevices_ids, 'device_id')
             devices_tags = self.get_target_col_vals_list_by_known_col_val(SystemDevice,'id', systemdevices_ids, 'tag')
@@ -74,7 +65,7 @@ class ProjectDetailController:
             devices_descs = self.get_target_col_vals_list_by_known_col_val(Device,'id',devices_ids,'description')
             devices_manfs = self.get_target_col_vals_list_by_known_col_val(Device,'id',devices_ids,'manufacturer')
             devices_models = self.get_target_col_vals_list_by_known_col_val(Device,'id',devices_ids,'model')
-            self.systems_devices_data_dict[system_id] = {'devices_tags' : {'data':devices_tags, 'max_char':0},
+            self.systems_devices_data_dict[system_key] = {'devices_tags' : {'data':devices_tags, 'max_char':0},
                                                          'devices_qtys' : {'data':devices_qtys, 'max_char':0},
                                                           'devices_descs' : {'data':devices_descs, 'max_char':0},
                                                           'devices_manfs' : {'data':devices_manfs, 'max_char':0},
--- project_detail/projectdetail_view.py
+++ project_detail/projectdetail_view.py
@@ -1,6 +1,6 @@
 from class_collection import BaseWindow
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, IntVar
 
 class ProjectDetailWindow(BaseWindow):
     def __init__(self, title, parent, controller, project_number, is_root=False):
@@ -11,8 +11,8 @@ class ProjectDetailWindow(BaseWindow):
         self.base_frame.columnconfigure(0, weight=1)
         self.create_project_label()
         self.create_system_base_frame()
-        self.controller.get_systems_devices_data()
-        # self.iter_generate_system_frame()
+        self.systems_devices_data_dict = self.controller.get_systems_devices_data()
+        self.iter_generate_system_frame()
         
 
         BaseWindow.center_window(self.root)
@@ -29,13 +29,21 @@ class ProjectDetailWindow(BaseWindow):
         self.system_base_frame.columnconfigure(0, weight=1)
         return self.system_base_frame
 
-    def create_system_frame(self, row_idx, system_name, system_id=None):        
+    def iter_generate_system_frame(self):        
+        if self.systems_devices_data_dict:
+            for idx, system_key in enumerate(self.systems_devices_data_dict.keys()):
+                self.create_system_frame(idx, system_key)
+        else:
+            self.create_add_system_frame(0)
+
+    def create_system_frame(self, row_idx, system_key):
+        system_name = system_key[1]
         system_frame = ttk.LabelFrame(self.system_base_frame, text=system_name)
         ypad = 10 if row_idx == 0 else (0,10)
         system_frame.grid(row=row_idx, column=0, padx=10, pady=ypad, sticky='nsew')        
         system_frame.columnconfigure(0, weight=1)
         device_base_frame = self.create_device_base_frame(system_frame)
-        self.iter_generate_device_frame(device_base_frame, system_id)
+        self.iter_generate_device_frame(device_base_frame, system_key)
 
     def create_add_system_frame(self, row_idx):
         system_frame = ttk.LabelFrame(self.system_base_frame, text='Add System')
@@ -43,14 +51,6 @@ class ProjectDetailWindow(BaseWindow):
         system_frame.grid(row=row_idx, column=0, padx=10, pady=ypad)
         system_add_label = ttk.Label(system_frame, text='Click here to add a new system.')      
         system_add_label.grid(row=0,column=0,padx=10,pady=10)
-
-    def iter_generate_system_frame(self):        
-        system_names, system_ids = self.controller.get_systems_names_ids_list()
-        if system_names:
-            for idx, (system_name, system_id) in enumerate(zip(system_names, system_ids)):
-                self.create_system_frame(idx,system_name, system_id)
-        else:
-            self.create_add_system_frame(0)
 
     def create_device_base_frame(self, parent):
         device_base_frame = ttk.LabelFrame(parent, text='Devices')
@@ -63,31 +63,31 @@ class ProjectDetailWindow(BaseWindow):
         device_add_label = ttk.Label(parent, text='Click here to add a new device.')      
         device_add_label.grid(row=0,column=0,padx=10,pady=ypad)
 
-    def iter_generate_device_frame(self, parent, system_id):        
-        devices_data_dict = self.controller.get_system_devices_data(system_id)
-        if devices_data_dict:
-            num_devices = len(devices_data_dict[system_id]['devices_models']['data'])            
+    def iter_generate_device_frame(self, parent, system_key):
+        
+        if self.systems_devices_data_dict[system_key]:
+            manufs_list = self.systems_devices_data_dict[system_key]['devices_manfs']['data']
+            num_devices = len(manufs_list)
             for row_idx in range(num_devices):
-                self.create_device_frame(parent, row_idx, system_id, devices_data_dict)
+                self.create_device_frame(parent, row_idx, system_key)
         else:
+            print('add emptyness')
             self.create_add_device_frame(parent, 0)
 
-    def create_device_frame(self, parent, row_idx, system_id, devices_data_dict):
+    def create_device_frame(self, parent, row_idx, system_key):
         device_frame = ttk.Frame(parent, relief='solid')
         ypad = 10 if row_idx == 0 else (0,10)
         device_frame.grid(row=row_idx,column=0,padx=10,pady=ypad,sticky='nsew')
-        device_frame.grid_columnconfigure((1,2,3), weight=1)
-        tag_data = devices_data_dict[system_id]['devices_tags']['data'][row_idx]
+        device_frame.grid_columnconfigure((1,2,3), weight=1)        
+        self.create_device_tag_entry(device_frame, row_idx, system_key)
+        self.create_device_label(device_frame, row_idx, system_key, 'devices_descs', 1)
+        self.create_device_label(device_frame, row_idx, system_key, 'devices_manfs', 2)
+        self.create_device_label(device_frame, row_idx, system_key, 'devices_models', 3)
+        self.create_device_spinbox(device_frame, row_idx, system_key, 'devices_qtys', 4)
 
-        self.create_device_tag_entry(device_frame, tag_data)
-        desc_data = devices_data_dict[system_id]['devices_descs']['data'][row_idx]
-        self.create_device_label(device_frame, desc_data, 1)
-        manf_data = devices_data_dict[system_id]['devices_manfs']['data'][row_idx]
-        self.create_device_label(device_frame, manf_data, 2)
-        model_data = devices_data_dict[system_id]['devices_models']['data'][row_idx]
-        self.create_device_label(device_frame, model_data, 3)
 
-    def create_device_tag_entry(self, parent, device_tag_data):
+    def create_device_tag_entry(self, parent, row_idx, system_key):
+        device_tag_data = self.systems_devices_data_dict[system_key]['devices_tags']['data'][row_idx]
         def validate_input_length(P):
             if len(P) > 7:  # Limit to 10 characters
                 return False
@@ -98,6 +98,15 @@ class ProjectDetailWindow(BaseWindow):
         device_tag_entry.delete(0, tk.END)
         device_tag_entry.insert(0, device_tag_data)
 
-    def create_device_label(self, parent, device_data, col):
-        device_desc_label = ttk.Label(parent, text=device_data, relief='solid')
-        device_desc_label.grid(row=0,column=col, stick='nsew')
+    def create_device_label(self, parent, row_idx, system_key, dev_prop_key, col):
+        device_data = self.systems_devices_data_dict[system_key][dev_prop_key]['data'][row_idx]
+        label_width = self.systems_devices_data_dict[system_key][dev_prop_key]['max_char'] + 2
+        device_label = ttk.Label(parent, text=f' {device_data}', relief='solid', width=label_width)
+        device_label.grid(row=0,column=col, stick='nsew')
+    
+    def create_device_spinbox(self, parent, row_idx, system_key, dev_prop_key, col):
+        device_data = self.systems_devices_data_dict[system_key][dev_prop_key]['data'][row_idx]
+        label_width = self.systems_devices_data_dict[system_key][dev_prop_key]['max_char'] 
+        device_spinbox = ttk.Spinbox(parent, from_=0, to=9999, width=label_width)
+        device_spinbox.set(device_data)
+        device_spinbox.grid(row=0,column=col, sticky='nsew')
